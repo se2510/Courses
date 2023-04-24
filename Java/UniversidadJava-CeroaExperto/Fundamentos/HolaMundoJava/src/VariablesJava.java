a5cca867b781b4d5694f006c75f72d9f5fe30946
David Elias González García <elias10david@hotmail.com>
2023-04-23 22:38:46 -0600

    Otra vez checando XD
    asies, asi soy yo

---
+++ Java/UniversidadJava-CeroaExperto/Fundamentos/HolaMundoJava/src/VariablesJava.java
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @author elias
+ * Mi clase en Java
+ */
+public class VariablesJava {
+    public static void main(String args[]){
+        // Definimos la variable
+        int miVariableEntera =  10;
+        System.out.println(miVariableEntera);
+        // Modificamos el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera); // sout y luego tab, netbeans autocompleta toda la linea de impresion
+        
+        String miVariableCadena = "Saluditos :D";
+        System.out.println(miVariableCadena); // Ctrl + espacio, nos indica el IDE las variables que hay (para autocompletar)
+        
+        miVariableCadena = "Bai bai :D";
+        System.out.println(miVariableCadena);
+    
+    }
+}
